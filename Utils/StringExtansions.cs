a59589e4f073e2e5652418d8c40617e817c24bf4
NVV <uniondirfolder@gmail.com>
2021-04-11 01:47:32 +0300

    add utils

---
+++ Utils/StringExtansions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class StringExtansions
+    {
+        public static bool IsGoodString(this string checkString) 
+        {
+            return string.IsNullOrEmpty(checkString) && string.IsNullOrWhiteSpace(checkString);
+        }
+    }
+}
