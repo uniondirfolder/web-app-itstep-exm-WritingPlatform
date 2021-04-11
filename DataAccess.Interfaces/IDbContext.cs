41b25ccdb6e47f6b55d00485fcf73af662d8cdba
NVV <uniondirfolder@gmail.com>
2021-04-11 13:21:11 +0300

    EF =  pattern Repository&UnitOfWork

---
+++ DataAccess.Interfaces/IDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    public interface IDbContext
+    {
+        DbSet<Member> Members { get; }
+        DbSet<MemberComment> MemberComments { get; }
+        DbSet<MemberStory> MemberStories { get; }
+        DbSet<StoryGenre> StoryGenres { get; }
+        DbSet<StoryRating> StoryRatings { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken token);
+    }
+}
